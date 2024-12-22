5a4c1d3d70b99f7e2a39fa788403d724ba70cb24
nexryai <nexryai@sda1.net>
2024-12-22 20:45:39 +0900

    API: Add offset

--- src/app/gallery/[id]/page.tsx
+++ src/app/gallery/[id]/page.tsx
@@ -9,9 +9,9 @@ export default function Page({params,}: {
     params: Promise<{ id: string }>
 }) {
     const [text, setText] = useState("");
-    const fetchGallery = async () => {
+    const fetchGallery = async (offset: number = 0) => {
         const id = (await params).id;
-        const { data } = await app.api.gallery({id}).get();
+        const { data } = await app.api.gallery({id}).get({query: {offset}});
         setText(JSON.stringify(data));
     };
 
