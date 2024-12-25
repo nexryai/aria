4def0acb5cf979aaa7dbdcdfb6969e1381ecce5b
nexryai <nexryai@sda1.net>
2024-12-25 11:41:11 +0900

    WIP: ギャラリー画面をグリッド表示できるように

--- src/app/gallery/[id]/page.tsx
+++ src/app/gallery/[id]/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 import { Button } from "antd";
+import { motion } from "motion/react";
 
 import { app } from "@/browser/api";
 import { blurHashToDataURL } from "@/browser/blurhash";
@@ -14,6 +15,21 @@ export default function Page({params,}: {
 }) {
     const [id, setId] = useState("");
     const [gallery, setGallery] = useState<GalleryWithImages | null>(null);
+
+    const blurInVariants = {
+        hidden: {
+            opacity: 0,
+            filter: "blur(10px)",
+        },
+        visible: {
+            opacity: 1,
+            filter: "blur(0px)",
+            transition: {
+                duration: 0.4, // アニメーションの継続時間
+                ease: "easeOut",
+            },
+        },
+    };
 
     const fetchGallery = async (offset: number = 0) => {
         const id = (await params).id;
@@ -41,36 +57,46 @@ export default function Page({params,}: {
     }, [gallery]);
 
     return (
-        <>
+        <div className="w-[80%] mx-auto">
             <Link href={`/gallery/${id}/upload`}><Button type="default">Upload</Button></Link>
 
-            <div className="mt-8">
+            <div className="mt-8 flex flex-wrap">
                 {gallery ? gallery.images.map((image) => (
-                    <div key={image.id}>
-                        <img id={`blurhash-${image.id}`} className="w-32 h-32 object-cover" width={image.width} height={image.height} />
-                        <img
-                            id={`thumbnail-${image.id}`}
-                            className="hidden w-32 h-32 object-cover"
-                            src={`/api/thumbnail/${image.thumbnailKey}`}
-                            alt="Thumbnail of gallery image"
-                            onLoad={
-                                () => {
-                                    // ロード完了時にblurhashを非表示にしてサムネイルを表示
-                                    const thumb = document.getElementById(`thumbnail-${image.id}`) as HTMLImageElement;
-                                    const img = document.getElementById(`blurhash-${image.id}`) as HTMLImageElement;
-                                    if (img) {
-                                        img.style.display = "none";
-                                    }
+                    <div key={image.id} className="relative w-32 h-32 overflow-hidden m-1">
+                        <img id={`blurhash-${image.id}`} className="w-32 h-32 object-cover absolute top-0 left-0" width={image.width} height={image.height}/>
 
-                                    thumb!.style.display = "block";
+                        <motion.div
+                            initial="hidden"
+                            animate="visible"
+                            variants={blurInVariants}
+                        >
+                            <img
+                                id={`thumbnail-${image.id}`}
+                                className="hidden w-32 h-32 object-cover absolute top-0 left-0"
+                                src={`/api/thumbnail/${image.thumbnailKey}`}
+                                alt="Thumbnail of gallery image"
+                                onLoad={
+                                    () => {
+                                        // ロード完了時にblurhashを非表示にしてサムネイルを表示
+                                        const thumb = document.getElementById(`thumbnail-${image.id}`) as HTMLImageElement;
+                                        const blurhash = document.getElementById(`blurhash-${image.id}`) as HTMLImageElement;
+                                        if (blurhash) {
+                                            setTimeout(() => {
+                                                blurhash.style.display = "none";
+                                            }, 500);
+                                        }
+
+                                        thumb!.style.display = "block";
+                                    }
                                 }
-                            }/>
+                            />
+                        </motion.div>
 
                     </div>
                 )) : (
                     <div>Loading...</div>
                 )}
             </div>
-        </>
+        </div>
     );
 }
