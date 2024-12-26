b9a4ecb158c9c7e789139daaf5758ff0f230a813
nexryai <nexryai@sda1.net>
2024-12-26 13:35:52 +0900

    maxRequestBodySizeをデフォルト値（128MB）から下げる

--- src/controllers/AppController.ts
+++ src/controllers/AppController.ts
@@ -33,7 +33,7 @@ const userService = new UserService(userRepository);
 const passkeyAuthService = new PasskeyAuthService(passkeyRepository);
 const galleryService = new GalleryService(galleryRepository, imageRepository, s3);
 
-export const authRouter = new Elysia({ prefix: "/auth" })
+export const authRouter = new Elysia({ prefix: "/auth", serve: { maxRequestBodySize: 1024 *  1024 * 4 } })
     .use(errorHandler)
     .post("/register-request", async ({body, cookie: {challengeSession}}) => {
         const user = await userService.createUser({name: body.displayName});
@@ -118,7 +118,7 @@ export const authRouter = new Elysia({ prefix: "/auth" })
     });
 
 
-export const apiRouter = new Elysia({ prefix: "/api" })
+export const apiRouter = new Elysia({ prefix: "/api", serve: { maxRequestBodySize: 1024 *  1024 * 4 }  })
     .use(errorHandler)
     .derive(({ cookie: {token} }) => {
         // Auth middleware
