87b2bc2e1b17bd09a76267087c9024ea8d91d9e2
nexryai <nexryai@sda1.net>
2024-12-27 09:33:18 +0900

    clean up

--- src/services/GalleryService.ts
+++ src/services/GalleryService.ts
@@ -1,6 +1,3 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
 import { type IGalleryRepository, IImageRepository } from "@/prisma";
 import { GallerySummary } from "@/schema/api";
 import { type StorageService } from "@/services/internal/StorageService";
