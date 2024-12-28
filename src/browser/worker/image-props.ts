b132b7c9673940023b50ef7b4554a9b0432a4420
nexryai <nexryai@sda1.net>
2024-12-28 14:17:44 +0900

    wasmでpanicになると永遠に進まなくなる問題を修正

--- src/browser/worker/image-props.ts
+++ src/browser/worker/image-props.ts
@@ -6,17 +6,31 @@ self.onmessage = async (event: MessageEvent) => {
 
     try {
         await wasmInitialized;
-
         console.log("[ImagePropsWorker] Getting image properties...");
-        const imageProps: {
-            width: number;
-            height: number;
-            blurhash: string;
-            checksum: string;
-        } = await getImageProps(file);
 
-        self.postMessage(imageProps);
+        const fetchProps = async () => {
+            const imageProps: {
+                width: number;
+                height: number;
+                blurhash: string;
+                checksum: string;
+            } = await getImageProps(file);
+
+            self.postMessage(imageProps);
+        };
+
+        // 現状WASMで発生したpanicをキャッチする方法がない
+        // https://rajrajhans.com/2023/07/handling-rust-panics-in-wasm/
+        await Promise.race([
+            fetchProps(),
+            new Promise((_, reject) => {
+                setTimeout(() => {
+                    reject(new Error("Image processing timed out"));
+                }, 20000);
+            }),
+        ]);
     } catch (error) {
+        console.log("WASM error:", error);
         self.postMessage({ error: (error as Error).message });
     }
 };
