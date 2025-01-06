ea3c9b5a8289bed9f33978cabf4a1bb75269e079
nexryai <nexryai@sda1.net>
2025-01-06 15:40:44 +0900

    Fix CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
+        // Playwrightと競合する
+        exclude: ["test/**/*.spec.ts"],
         sequence: {
             shuffle: false,
             concurrent: false,
