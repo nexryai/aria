03c8d365f7bc35a3ee9d6582c2c850c819df69e2
nexryai <nexryai@sda1.net>
2025-01-31 23:59:36 +0900

    Nodeで動くように

--- src/controllers/AppController.ts
+++ src/controllers/AppController.ts
@@ -36,7 +36,7 @@ const userService = new UserService(userRepository);
 const passkeyAuthService = new PasskeyAuthService(passkeyRepository);
 const galleryService = new GalleryService(galleryRepository, imageRepository, storageService);
 
-export const authRouter = new Elysia({ prefix: "/auth", serve: { maxRequestBodySize: 1024 *  1024 * 4 } })
+export const authRouter = new Elysia({ prefix: "/auth", serve: { maxRequestBodySize: 1024 *  1024 * 4 }, aot: false })
     .use(errorHandler)
     .post("/register-request", async ({body, cookie: {challengeSession}}) => {
         if (process.env.NODE_ENV === "production" && process.env.ALLOW_REGISTRATION !== "1") {
@@ -127,7 +127,7 @@ export const authRouter = new Elysia({ prefix: "/auth", serve: { maxRequestBodyS
     });
 
 
-export const apiRouter = new Elysia({ prefix: "/api", serve: { maxRequestBodySize: 1024 *  1024 * 4 } })
+export const apiRouter = new Elysia({ prefix: "/api", serve: { maxRequestBodySize: 1024 *  1024 * 4 }, aot: false })
     .use(errorHandler)
     .derive(({ cookie: {token} }) => {
         // Auth middleware
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,85 @@
 import { Elysia } from "elysia";
 
+import { IncomingHttpHeaders } from "node:http";
+
+import { node } from "@elysiajs/node";
+import Polka from "polka";
+
 import { apiRouter, authRouter } from "@/controllers/AppController";
 
 
+function incomingHttpHeadersToRequestHeaders(headers:  IncomingHttpHeaders) {
+    const requestHeaders = new Headers();
+    for (const [key, value] of Object.entries(headers)) {
+        requestHeaders.set(key, value as string);
+    }
+    return requestHeaders;
+}
+
 console.log("Starting server...");
-const app = new Elysia()
+const app = new Elysia({ aot: false })
     .use(authRouter)
     .use(apiRouter);
 
-app.listen(3000);
+const server = Polka().use(async (req, res) => {
+    const requestUrl = (req.protocol || "http") + "://" + (req.host || req.headers.host || req.headers.origin) + req.url;
+    console.log(`[${req.method}]`, requestUrl);
+    const webStandardRequest = new Request(requestUrl, {
+        method: req.method,
+        headers: incomingHttpHeadersToRequestHeaders(req.headers),
+        body: req.body,
+    });
+
+    const webStandardRes = await app.fetch(webStandardRequest);
+
+    for (const [key, value] of webStandardRes.headers.entries()) {
+        res.setHeader(key, value);
+    }
+
+    res.writeHead(webStandardRes.status);
+
+    if (!webStandardRes.body) {
+        res.end();
+        return;
+    }
+
+    const reader = webStandardRes.body.getReader();
+    if (res.destroyed) {
+        reader.cancel();
+        return;
+    }
+
+    const cancel = (error: Error) => {
+        res.off("close", cancel);
+        res.off("error", cancel);
+
+        reader.cancel(error).catch(() => {});
+        if (error) res.destroy(error);
+    };
+
+    res.on("close", cancel);
+    res.on("error", cancel);
+
+    next();
+    async function next() {
+        try {
+            for (;;) {
+                const { done, value } = await reader.read();
+
+                if (done) break;
+
+                if (!res.write(value)) {
+                    res.once("drain", next);
+                    return;
+                }
+            }
+            res.end();
+        } catch (error) {
+            cancel(error instanceof Error ? error : new Error(String(error)));
+        }
+    }
+});
+
+app.compile();
+server.listen(3000);
 console.log("Server started.");
